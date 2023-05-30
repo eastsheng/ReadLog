2f95ace7b0d74e255388a5eeb52bfccd455253fa
eastsheng <eastsheng@hotmail.com>
2023-05-30 21:54:23 +0800

    update ReadLog-1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ python setup.py sdist
 pip install .
 """
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
 name         = 'ReadLog',
@@ -11,6 +12,9 @@ version      = '1.1.0',
 py_modules   = ['ReadLog'],
 author       = 'CHENDONGSHENG',
 author_email = 'eastsheng@hotmail.com',
+packages=find_packages('src'),
+package_dir={'': 'src'},
+install_requires=open('requirements.txt').readlines(),
 url          = 'https://github.com/eastsheng',
 description  = 'Read themo info from lammps output file or log file'
 )
