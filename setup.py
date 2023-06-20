9104d0201dab954296b220ad7e4e0bce0039acb4
eastsheng <eastsheng@hotmail.com>
2023-06-20 15:43:13 +0800

    update 1.1.8

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt","r") as f:
 
 setup(
 name         = 'readlog',
-version      = '1.1.7',
+version      = '1.1.8',
 py_modules   = ['readlog'],
 author       = 'CHENDONGSHENG',
 author_email = 'eastsheng@hotmail.com',
