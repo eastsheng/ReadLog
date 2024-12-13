55626bba7a9dda21f6a20488fdea4dd67c466d74
eastsheng <eastsheng@hotmail.com>
2024-12-13 21:43:24 +0800

    add printlog

--- src/readlog.py
+++ src/readlog.py
@@ -6,6 +6,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+from datetime import datetime
+from pathlib import Path
+import logging
+import sys
 
 def __version__():
 	version = "1.2.3"
@@ -183,6 +187,39 @@ class ReadLog(object):
 				except:
 					print("Warning: No 'timestep' in Your Logfile...\n\nPlease check it.")
 					return None
+
+
+
+
+
+class Tee:
+	def __init__(self, *files):
+		self.files = files
+
+	def write(self, obj):
+		for f in self.files:
+			f.write(obj)
+
+	def flush(self):
+		for f in self.files:
+			f.flush()
+
+def print_log(log_file_name):
+	current_time = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+	log_file_name = f"{log_file_name}"
+	logging.basicConfig(filename=log_file_name, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+	log_file = open(log_file_name, 'a')
+	sys.stdout = Tee(sys.stdout, log_file)
+	print("-"*100)
+	print("-"*100)
+	print(f">>> Date and Time: {current_time}")
+	print(f">>> logfile name: {log_file_name}")
+	print("-"*100)
+	print("-"*100)
+	return
+
+
+
 
 
 if __name__ == '__main__':
